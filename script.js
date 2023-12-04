6acb35ed58dcca8687127bddd25cd1e0940a34a4
Dima <dimagalai04@gmail.com>
2023-12-04 13:45:28 +0200

    project added

---
+++ script.js
@@ -0,0 +1,99 @@
+const canvas = document.getElementById("game");
+const space = canvas.getContext("2d");
+let box = 32;
+let score = 0;
+let highScore= 0
+let snake = [];
+let game;
+let dir;
+
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const foodImage = new Image();
+foodImage.src = "img/dota.png";
+
+const snakeHead = new Image();
+snakeHead.src = "img/pudge.jpg"
+
+const body = new Image();
+body.src = "img/body.jpg"
+
+const randomCoord = () => { return Math.floor(Math.random() * 15 + 3) * box };
+
+let food = {
+  x: randomCoord(),
+  y: randomCoord()
+};
+
+snake[0] = {
+    x: 15 * box,
+    y: 15 * box
+};
+
+document.addEventListener("keydown", direction);
+
+function direction(event){
+    if (event.keyCode === 37 && dir !== "right"){
+        dir = "left"
+    } else if (event.keyCode === 38 && dir !== "down"){
+        dir = "up"
+    } else if (event.keyCode === 39 && dir !== "left"){
+    dir = "right"
+    } else if (event.keyCode === 40 && dir !== "up"){
+    dir = "down"
+    }
+}
+const gameOver = (head, arr) => {
+    for (let i = 0; i < snake.length; i++){
+        if (head.x === arr[i].x && head.y === arr[i].y) return true;
+    }
+}
+
+const handleGameOver = () => {
+    clearInterval(game);
+    alert('Game Over! Press OK to try again...');
+    location.reload();
+};
+function initGame() {
+    space.drawImage(ground, 0, 0);
+    space.drawImage(foodImage, food.x, food.y);
+    space.drawImage(snakeHead, snake[0].x, snake[0].y);
+
+    for (let i = 1; i < snake.length; i++){
+        space.drawImage(body, snake[i].x, snake[i].y);
+    }
+    space.fillStyle = "white";
+    space.font = "25px Comic Sans MS";
+    space.fillText(`Score: ${score}`, box * 2.4, box * 1.5);
+    localStorage.setItem('high-score', highScore = score > highScore ? score : highScore);
+    space.fillText(`High Score: ${highScore}`, box * 13, box * 1.5)
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(snakeX === food.x && snakeY === food.y){
+        score++;
+        food = {
+            x: Math.floor(Math.random() * 17 + 1) * box,
+            y: Math.floor(Math.random() * 15 + 3) * box
+        };
+    } else snake.pop();
+
+    if(snakeX < box || snakeX > box * 17 || snakeY < 3 * box || snakeY > box * 17) handleGameOver();
+
+    if (dir === "left") snakeX -= box;
+    if (dir === "right") snakeX += box;
+    if (dir === "up") snakeY -= box;
+    if (dir === "down") snakeY += box;
+
+    let head = {
+        x: snakeX,
+        y: snakeY
+    };
+    if (gameOver(head, snake)) handleGameOver()
+
+    snake.unshift(head);
+}
+
+game = setInterval(initGame, 100);
